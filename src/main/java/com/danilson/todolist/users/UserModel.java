fed186cb05be33221a1ec40a5d682345339c64f6
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2023-10-11 21:56:36 +0100

    iniciando projeto spring Boot

---
+++ src/main/java/com/danilson/todolist/users/UserModel.java
@@ -0,0 +1,12 @@
+package com.danilson.todolist.users;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String username;
+    private String name;
+    private String password;
+
+} 
